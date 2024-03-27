cce4006434278aadaab7c6fc6daecc8c64ed64be
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-03-27 23:17:51 +0530

    created general user schema

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String,
+      enum: ["Student", "Teacher", "Admin"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
