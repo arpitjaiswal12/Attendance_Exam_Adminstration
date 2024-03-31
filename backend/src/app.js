4360f07c51f5d42d0ca6618750b65932a1d6fc33
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-03-31 13:39:49 +0530

    configure app and dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,24 @@
 import express from "express";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // extended is use for object within object
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//importing routing
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
